6e2c013cab183aae2e3203a929153f6031d22dc8
tusindfryd <tusindfryd@users.noreply.github.com>
2023-01-21 11:04:14 +0100

    slight refactor

--- src/turtlebot_controller/turtlebot_controller/camera_node.py
+++ src/turtlebot_controller/turtlebot_controller/camera_node.py
@@ -6,30 +6,6 @@ import cv2
 from cv_bridge import CvBridge
 import numpy as np
 
-# ARUCO_DICT = {
-#     "DICT_4X4_50": cv2.aruco.DICT_4X4_50,
-#     "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
-#     "DICT_4X4_250": cv2.aruco.DICT_4X4_250,
-#     "DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
-#     "DICT_5X5_50": cv2.aruco.DICT_5X5_50,
-#     "DICT_5X5_100": cv2.aruco.DICT_5X5_100,
-#     "DICT_5X5_250": cv2.aruco.DICT_5X5_250,
-#     "DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
-#     "DICT_6X6_50": cv2.aruco.DICT_6X6_50,
-#     "DICT_6X6_100": cv2.aruco.DICT_6X6_100,
-#     "DICT_6X6_250": cv2.aruco.DICT_6X6_250,
-#     "DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
-#     "DICT_7X7_50": cv2.aruco.DICT_7X7_50,
-#     "DICT_7X7_100": cv2.aruco.DICT_7X7_100,
-#     "DICT_7X7_250": cv2.aruco.DICT_7X7_250,
-#     "DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
-#     "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
-#     "DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
-#     "DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
-#     "DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
-#     "DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11,
-# }
-
 
 class ExamineImage(Node):
     def __init__(self):
@@ -49,7 +25,6 @@ class ExamineImage(Node):
     def image_callback(self, image_data):
         self.image = CvBridge().imgmsg_to_cv2(image_data, "bgr8")
         self.image_size = (image_data.height, image_data.width)
-
         if self.point is not None:
             cv2.rectangle(
                 self.image,
@@ -58,7 +33,6 @@ class ExamineImage(Node):
                 (0, 255, 0),
                 3,
             )
-
         cv2.setMouseCallback(self.window_name, self.handle_pointer)
 
         markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(
@@ -73,38 +47,30 @@ class ExamineImage(Node):
             cv2.imshow(self.window_name, self.image)
             cv2.waitKey(10)
 
+    def move(self, indicator):
+        msg = Twist()
+        if indicator > self.image_size[0] / 2:
+            msg.linear.x = -0.5
+        else:
+            msg.linear.x = 0.5
+        self.publisher.publish(msg)
+
     def handle_pointer(self, event, x, y, flags, param):
         self.point = (x, y)
         if event == cv2.EVENT_LBUTTONDOWN:
-            msg = Twist()
-            if y > self.image_size[0] / 2:
-                msg.linear.x = -0.5
-            else:
-                msg.linear.x = 0.5
-
-            self.publisher.publish(msg)
+            self.move(y)
 
     def handle_aruco(self, corners, ids, rejected, image):
         if corners:
             ids = ids.flatten()
-
             for (markerCorner, markerID) in zip(corners, ids):
                 corners = np.array(markerCorner, np.int32)
                 (topLeft, topRight, bottomRight, bottomLeft) = corners.reshape((4, 2))
                 cv2.polylines(image, [corners], True, (0, 0, 255), 2)
-
                 cX = int((topLeft[0] + bottomRight[0]) / 2.0)
                 cY = int((topLeft[1] + bottomRight[1]) / 2.0)
                 cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
-
-                msg = Twist()
-                if cY > self.image_size[0] / 2:
-                    msg.linear.x = -0.5
-                else:
-                    msg.linear.x = 0.5
-
-                self.publisher.publish(msg)
-
+                self.move(cY)
         return image
 
 
